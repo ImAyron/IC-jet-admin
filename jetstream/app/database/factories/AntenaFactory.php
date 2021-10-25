e9041724037d5d0c6b62c03cff771da0c7f78d0d
Bruno Passamai <b.passamai@gmail.com>
2021-10-25 20:44:27 -0300

    update 25-10

---
+++ jetstream/app/database/factories/AntenaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Antena;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AntenaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Antena::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
