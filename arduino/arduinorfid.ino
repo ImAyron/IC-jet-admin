a87dc668693c9f710c4d648adbf6b96f91e9963c
Ayron Luigi de paiva <49703457+ImAyron@users.noreply.github.com>
2022-02-13 03:03:33 -0300

    Atualização codigo arduino

--- arduino/arduinorfid.ino
+++ arduino/arduinorfid.ino
@@ -2,26 +2,31 @@
 #include <MFRC522.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
+#include <iostream>
+#include <string>
 
-constexpr uint8_t RST_PIN = D3;     // Configuração dos cabos 
-constexpr uint8_t SS_PIN = D4;     // Configuração dos cabos 
+constexpr uint8_t RST_PIN = D3;     // Configuração dos cabos
+constexpr uint8_t SS_PIN = D4;     // Configuração dos cabos
 MFRC522 rfid(SS_PIN, RST_PIN); // Instância da classe MFRC5222
 MFRC522::MIFARE_Key key;
 String tag;
 #define ON_Board_LED 2
 
 //----------------------------------------SSID e Senha do Routeador-------------------------------------------------------------------------------------------------------------//
-const char* ssid = "Alvarenga";
-const char* password = "mariana18";
+const char* ssid = "Rep.KALANGO";
+const char* password = "porekada";
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 ESP8266WebServer server(80);  //--> Server na porta 80
+String tipo="aa";
+
+
 
 int readsuccess;
 byte readcard[4];
 char str[32] = "";
 String StrUID;
- WiFiClient client;
+WiFiClient client;
 
 void setup() {
 
@@ -33,7 +38,7 @@ void setup() {
 
   WiFi.begin(ssid, password); //--> Connect to your WiFi router
   Serial.println("");
-  pinMode(ON_Board_LED,OUTPUT); 
+  pinMode(ON_Board_LED, OUTPUT);
   digitalWrite(ON_Board_LED, HIGH); //--> Turn off Led On Board
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -59,60 +64,72 @@ void setup() {
 }
 void loop() {
 
+    
+         
+
   readsuccess = getid();
-  if(readsuccess) {  
+  if (readsuccess) {
     digitalWrite(ON_Board_LED, LOW);
     HTTPClient http;    //Declare object of class HTTPClient
     String UIDresultSend, postData;
     UIDresultSend = StrUID;
-   
+
     //Post Data
     postData = UIDresultSend;
-  
-    http.begin(client, "http://192.168.2.12:8000/api/operation");  //Specify request destination
+    if (tipo=="aa" || tipo=="Saida") {
+                tipo = "Entrada";
+                
+    }
+    else {
+      if(tipo=="Entrada"){
+                tipo = "Saida";    
+      }
+    }
+
+    http.begin(client, "http://192.168.0.12:8000/api/operation");  //Specify request destination
     http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //Specify content-type header
-    int httpCode = http.POST("operations="+postData);   //Send the request
+    int httpCode = http.POST("operations=" + postData+"&antena=lavanderia"+"&tipo="+tipo); //Send the request
     String payload = http.getString();    //Get the response payload
-  
+    Serial.println(tipo);
     Serial.println(UIDresultSend);
     Serial.println(httpCode);   //Print HTTP return code
     Serial.println(payload);    //Print request response payload
-    
+
     http.end();  //Close connection
     delay(1000);
-  digitalWrite(ON_Board_LED, HIGH);
+    digitalWrite(ON_Board_LED, HIGH);
   }
 
 
 }
-  int getid() { 
-    if ( ! rfid.PICC_IsNewCardPresent()){
-           return 0;
-    }
-   
-    if(!  rfid.PICC_ReadCardSerial()) {
-      return 0;
-    }
-   
-    Serial.print("THE UID OF THE SCANNED CARD IS : ");
-
-     for(int i=0;i<4;i++){
-      readcard[i]=rfid.uid.uidByte[i]; //storing the UID of the tag in readcard
-      array_to_string(readcard, 4, str);
-      StrUID = str;
-      }
-    rfid.PICC_HaltA();
-    return 1;
+int getid() {
+  if ( ! rfid.PICC_IsNewCardPresent()) {
+    return 0;
   }
+
+  if (!  rfid.PICC_ReadCardSerial()) {
+    return 0;
+  }
+
+  Serial.print("THE UID OF THE SCANNED CARD IS : ");
+
+  for (int i = 0; i < 4; i++) {
+    readcard[i] = rfid.uid.uidByte[i]; //storing the UID of the tag in readcard
+    array_to_string(readcard, 4, str);
+    StrUID = str;
+  }
+  rfid.PICC_HaltA();
+  return 1;
+}
 
 
 void array_to_string(byte array[], unsigned int len, char buffer[]) {
-    for (unsigned int i = 0; i < len; i++)
-    {
-        byte nib1 = (array[i] >> 4) & 0x0F;
-        byte nib2 = (array[i] >> 0) & 0x0F;
-        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
-        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
-    }
-    buffer[len*2] = '\0';
+  for (unsigned int i = 0; i < len; i++)
+  {
+    byte nib1 = (array[i] >> 4) & 0x0F;
+    byte nib2 = (array[i] >> 0) & 0x0F;
+    buffer[i * 2 + 0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+    buffer[i * 2 + 1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+  }
+  buffer[len * 2] = '\0';
 }
--- jetstream/app/resources/views/operation/index.blade.php
+++ jetstream/app/resources/views/operation/index.blade.php
@@ -11,11 +11,21 @@
 @section('content')
 
 <!-- Reload Página-->
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="./plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="./plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="./plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="./dist/css/adminlte.min.css">
+<br>
 
-
-<table class="table table-bordered table-hover table-striped table-sm table-responsive-sm">
+<table id="example1" class="table table-bordered table-striped table-responsive-sm">
     <caption>Ultimas leituras</caption>
-    <thead class="thead-dark">
+    <thead>
    
         <tr>
             <th scope="col">Tag</th>
@@ -45,9 +55,7 @@
     </tbody>
 </table>
 
-<div class="text-center">
-    <a href="{{ route('operacao.create')}}" class="btn btn-lg btn-primary">Cadastrar</a>
-</div>
+
 
 
 
@@ -58,5 +66,30 @@
 @stop
 
 @section('js')
-
+<!-- jQuery -->
+<script src="./plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="./plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="./plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="./plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="./plugins/pdfmake/pdfmake.min.js"></script>
+<script src="./plugins/pdfmake/vfs_fonts.js"></script>
+<script src="./plugins/jszip/jszip.min.js"></script>
+<!-- Page specific script -->
+<script>
+  $(document).ready(function() {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": true, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": true,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
 @stop
