7685442dae6b198b175c5dc31bf655f5d802ac9d
Ayron Luigi de paiva <49703457+ImAyron@users.noreply.github.com>
2023-01-12 02:32:49 -0300

    tradução tabela

--- jetstream/app/resources/views/item/index.blade.php
+++ jetstream/app/resources/views/item/index.blade.php
@@ -92,6 +92,9 @@
 <script>
   $(document).ready(function() {
     $("#example1").DataTable({
+      "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json"
+      },
       "responsive": true,
       "lengthChange": true,
       "autoWidth": true,
