a0852947868d8db516ac124025790375fba47f2d
Kevin Lucas <kevinbrito2012@gmail.com>
2024-06-06 17:51:05 -0300

    refact: improve filter styles and identation page

--- jetstream/app/resources/views/leitura/index.blade.php
+++ jetstream/app/resources/views/leitura/index.blade.php
@@ -23,108 +23,109 @@
 <link rel="stylesheet" href="./dist/css/adminlte.min.css">
 
 <div class="row">
-  <div class="col">
+  <div class="col"></div>
 
-  </div>
   <div class="col">
+    <p class="text-center fw-bold text-info">Tags Cadastradas {{$count}}</p>
+    <p class="text-center fw-bold text-danger">Total de tags {{$tagsLidas2}}</p>
 
-    <p style="color:blue;text-align:center;">Tags Cadastradas {{$count}}</p>
-    <p style="color:Red;text-align:center;">Total de tags {{$tagsLidas2}}</p>
     <select onchange="location.href=this.value" class="form-select form-select-lg mb-3" name="" id="">
       <option selected style="color: #17A2B8;">Tags do Sistema</option>
 
-
-      @foreach($tags as $t){
-
-      <option class="option" value="{{route('leitura.show',$tagName[$t->id][0]->id)}}">{{$t->codigo}} - {{$t->item->descricao}}</option>
-
-      }
-
+      @foreach($tags as $t)
+        <option class="option" value="{{route('leitura.show',$tagName[$t->id][0]->id)}}">
+            {{$t->codigo}} - {{$t->item->descricao}}
+        </option>
       @endforeach
     </select>
     <form action="{{ route('route_post.php') }}">
       @csrf
 
-      <label for="data">Data de Inicio:</label>
-      <input type="date" id="data" name="data">
-      <input type="time" name="time" id="time">
+      <label for="data" class="form-label">Data de Inicio:</label>
+      <input class="form-control form-control-sm" type="date" id="data" name="data">
+      <input class="form-control form-control-sm" type="time" name="time" id="time">
 
       <p></p>
-      <label for="data">Data de Fim:</label>
-      <input type="date" id="data2" name="data2">
-      <input type="time" name="time1" id="time1">
 
+      <label for="data" class="form-label">Data de Fim:</label>
+    <input class="form-control form-control-sm" type="date" id="data" name="data">
+    <input class="form-control form-control-sm" type="time" name="time" id="time">
 
-      <button type="submit">Enviar</button>
+      <button class="btn btn-primary mt-2 mb-2 float-right" type="submit">Enviar</button>
     </form>
   </div>
-  <div class="col">
 
-  </div>
-
-
+  <div class="col"></div>
 </div>
 
 
 <table id="example1" class="table table-bordered table-striped table-responsive-sm">
   <caption>Ultimas leituras</caption>
   <thead>
-
     <tr>
       <th scope="col">Id</th>
       <th scope="col">Tag</th>
       <th scope="col">Data</th>
       <th scope="col">Antena</th>
       <th scope="col">Tipo</th>
-
-
-
     </tr>
   </thead>
+
   <tbody>
-
     @foreach($leituras as $o)
-    <tr>
+        <tr>
+            <td>{{ $o->id}}</td>
 
-      <td>{{ $o->id}}</td>
+            <td>
+                <a class="text-info" href="{{ route('tag.show', $o->EPC) }}">{{$o->EPC}}</a>
+            </td>
+            <td> {{date( 'd/m/Y  H:i:s' , strtotime($o->Data))  }}</td>
+            <td>{{ $o->company_id }} </td>
 
-      <td><a class="text-info" href="{{ route('tag.show', $o->EPC) }}">{{$o->EPC}}</a></td>
-      <td> {{date( 'd/m/Y  H:i:s' , strtotime($o->Data))  }}</td>
-      <td>{{ $o->company_id }} </td>
-      <?php
-      if ($o->tipo == 'Entrada') { ?>
-        <td><span class="badge bg-success">{{ $o->tipo }}</span> </td>
-      <?php  } else { ?>
-
-
-        <td><span class="badge bg-danger">{{ $o->tipo }}</span> </td>
-      <?php  } ?>
-    </tr>
+            @php
+                $badgeClass = $o->tipo == 'Entrada' ? 'bg-success' : 'bg-danger';
+            @endphp
+            <td>
+                <span class="badge {{ $badgeClass }}">{{ htmlentities($o->tipo) }}</span>
+            </td>
+        </tr>
     @endforeach
   </tbody>
 </table>
-
-
-
-
 
 @stop
 
 @section('css')
 
 <style>
-  select:hover {
-    background-color: white;
-  }
-
-  .option {
+.form-select {
     color: white;
     background-color: #17A2B8;
-  }
+    border-color: #17A2B8bb;
+    border-radius: 0.25rem;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    transition: color 0.15s, background-color 0.15s, border-color 0.15s, box-shadow 0.15s;
+}
 
-  
+.form-select:hover {
+    color: white;
+    background-color: #68c1cf;
+    border-color: #68c1cf;
+    cursor: pointer;
+}
+
+select:hover {
+    background-color: white;
+}
+
+.option {
+    color: #17A2B8;
+    background-color: #fff;
+}
+
 #example1_wrapper .dt-buttons button {
-  
+
   background-color: #17A2B8; /* Substitua pela cor desejada */
   color: #fff; /* Cor do texto nos botões */
 }
@@ -136,7 +137,7 @@
 }
 
 
-  
+
 </style>
 @stop
 
@@ -162,7 +163,7 @@
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
 
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-   
+
   });
 </script>
 @stop
